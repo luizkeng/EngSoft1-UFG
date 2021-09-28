17129de7fe0fa51d07f6f39e5e8d8dbd42b10cd6
Luiz Yokoyama <57710684+disconhecido@users.noreply.github.com>
2021-09-28 10:14:49 -0300

    Add files via upload
    Classe Calculadora para manipular as operações da calculadora

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+class Calculadora:
+	'''Classe Calculadora para manipular as operações da calculadora'''
+	
+    def soma(self, a, b):
+		'''Retorna a soma de a e b'''
+        return a + b
+
+    def subtrai(self, a, b):
+		'''Retorna a subtração de a por b'''
+        return a - b
+
+    def multiplica(self, a, b):
+		'''Retorna a multiplicação de a por b'''
+        return a * b
+
+    def divide(self, a, b):
+		'''Retorna a divisão de a por b'''
+        return a / b
